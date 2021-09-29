89090c69cc11ac8c3a6a02ad64dccf1909de0e17
augohm <73218692+augohm@users.noreply.github.com>
2021-09-29 22:41:44 +0200

    Add files via upload
    Dette er filer som er en del af vores Unit test af programet, man finder test under test.

---
+++ src/com/company/Shapes.java
@@ -0,0 +1,12 @@
+package com.company;// Denne fil vil håndtere nogle af de gennemgående funktionaliteter som bruges ved de1 forskellige former. Dette er blandt andet smart at have hvis man senere ville tilføje nye figure.
+
+public abstract class Shapes {
+  public abstract double centerX();
+  public abstract double centerY();
+
+  public abstract double circumference();
+  public abstract double area(); 
+
+ public abstract boolean isPointInsideShape();
+}
+
