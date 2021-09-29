9b3bab9755edb84e07d1de2548cf32ebb5fc4514
augohm <73218692+augohm@users.noreply.github.com>
2021-09-29 22:47:36 +0200

    Add files via upload
    unit test

---
+++ TriangleTest.java
@@ -0,0 +1,28 @@
+package com.company;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class TriangleTest {
+
+    @Test
+    void area() {
+    }
+
+    @Test
+    void circumference() {
+    }
+
+    @Test
+    void isPointInsideShape() {
+    }
+
+    @Test
+    void centerX() {
+    }
+
+    @Test
+    void centerY() {
+    }
+}
